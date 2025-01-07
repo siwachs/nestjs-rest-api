0264f244fbadae74c272bc907fe6e6fd115e7ce0
siwachs <ssiwach.8888@gmail.com>
2025-01-07 22:40:33 +0530

    e2e test script

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,13 +3,23 @@ import { Test } from '@nestjs/testing';
 
 import { AppModule } from '../src/app.module';
 
+import { PrismaService } from '../src/prisma/prisma.service';
+
+import * as pactum from 'pactum';
+
+import { AuthDto } from '../src/auth/dto';
+
 // Nest JS take root module ie app.module and compile it and we load that compiled module and test it.
 // yarn test:e2e
 // Add --watch and --no-cache in script to do automatic test on change
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
+  const PORT = process.env.PORT ?? 8000;
+
+  // Initially
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -23,10 +33,111 @@ describe('App e2e', () => {
     );
 
     await app.init();
+    await app.listen(PORT);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+
+    pactum.request.setBaseUrl(`http://localhost:${PORT}`);
   });
 
+  // At last
   afterAll(() => {
     app.close();
   });
-  it.todo('Should Pass');
+
+  // Tests
+  describe('Auth', () => {
+    const dto: AuthDto = { email: 'email@gmail.com', password: 'password' };
+
+    describe('SignUp', () => {
+      it('Should throw error if email is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({ password: dto.password })
+          .expectStatus(400)
+          .inspect();
+      });
+
+      it('Should throw error if password is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({ email: dto.email })
+          .expectStatus(400)
+          .inspect();
+      });
+
+      it('Should throw error if email and passward is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({})
+          .expectStatus(400)
+          .inspect();
+      });
+
+      it('Should SignUp', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201)
+          .inspect();
+      });
+    });
+
+    describe('SignIn', () => {
+      // Similarly with sign in
+
+      it('Should throw error if credentials are wrong', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({})
+          .expectStatus(400)
+          .inspect();
+      });
+
+      it('Should SignIn', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .inspect()
+          .stores('access_token', 'access_token');
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {
+      it('Should get current logged in user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: `Bearer $S{access_token}`,
+          })
+          .expectStatus(200)
+          .inspect();
+      });
+    });
+
+    describe('Edit user', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+
+    describe('Get bookmarks', () => {});
+
+    describe('Get bookmark by id', () => {});
+
+    describe('Edit bookmark by id', () => {});
+
+    describe('Delete bookmark by id', () => {});
+  });
 });
